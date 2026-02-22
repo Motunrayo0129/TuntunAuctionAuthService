dd6a8fcaa152b67d56c6803dcfbc55a59b66ab81
Onibudo Motunrayo <onibudomotunrayo2901@gmail.com>
2026-02-23 00:52:12 +0100

    Adding email service to the auth-service

--- src/main/java/com/TuntunAuction/auth_service/services/EmailService.java
+++ src/main/java/com/TuntunAuction/auth_service/services/EmailService.java
@@ -1,4 +1,9 @@
 package com.TuntunAuction.auth_service.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface EmailService {
+
+    void sendVerificationEmail(String to, String fullname, String verificationCode);
 }
