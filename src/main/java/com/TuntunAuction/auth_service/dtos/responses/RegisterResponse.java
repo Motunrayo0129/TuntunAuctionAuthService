763837a24051e4065e3214b29ffc996d02679630
Onibudo Motunrayo <onibudomotunrayo2901@gmail.com>
2026-02-22 21:38:30 +0100

    Adding Register response fields to the dto

--- src/main/java/com/TuntunAuction/auth_service/dtos/responses/RegisterResponse.java
+++ src/main/java/com/TuntunAuction/auth_service/dtos/responses/RegisterResponse.java
@@ -1,17 +1,18 @@
 package com.TuntunAuction.auth_service.dtos.responses;
 
 import com.TuntunAuction.auth_service.data.models.Role;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class RegisterResponse {
 
-    private String refreshToken;
-    private String id;
+    private Long id;
     private String fullname;
     private String email;
     private String phoneNo;
     private Role role;
     private LocalDateTime createdAt;
-    private String verificationCode;
+
 }
