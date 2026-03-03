0a58c55065f721f902046735d7de03bf172b6e06
Onibudo Motunrayo <onibudomotunrayo2901@gmail.com>
2026-03-03 23:42:57 +0100

    Adding the handlers for the global exception

--- src/main/java/com/TuntunAuction/auth_service/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/TuntunAuction/auth_service/handlers/GlobalExceptionHandler.java
@@ -1,4 +1,46 @@
 package com.TuntunAuction.auth_service.handlers;
 
+import com.TuntunAuction.auth_service.exceptions.PasswordMismatchException;
+import com.TuntunAuction.auth_service.exceptions.UnauthorizedException;
+import com.TuntunAuction.auth_service.exceptions.UserAlreadyExistException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<String> handleUserAlreadyExist(UserAlreadyExistException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(PasswordMismatchException.class)
+    public ResponseEntity<String> handlePasswordMismatch(PasswordMismatchException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorized(UnauthorizedException e){
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationErrors(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+
+
+    }
+
+
+
 }
