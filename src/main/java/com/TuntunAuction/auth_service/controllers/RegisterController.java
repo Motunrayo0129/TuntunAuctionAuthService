6645bad9b98fa758d8f5bbc0370f7adaa0745f63
Onibudo Motunrayo <onibudomotunrayo2901@gmail.com>
2026-03-03 23:41:48 +0100

    Adding the Register controller to the controllers

--- src/main/java/com/TuntunAuction/auth_service/controllers/RegisterController.java
+++ src/main/java/com/TuntunAuction/auth_service/controllers/RegisterController.java
@@ -1,4 +1,30 @@
 package com.TuntunAuction.auth_service.controllers;
 
+import com.TuntunAuction.auth_service.dtos.requests.RegisterRequest;
+import com.TuntunAuction.auth_service.dtos.responses.RegisterResponse;
+import com.TuntunAuction.auth_service.services.RegisterService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+
 public class RegisterController {
+    private final RegisterService registerService;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register (@Valid @RequestBody RegisterRequest request) {
+        RegisterResponse response = registerService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+
 }
