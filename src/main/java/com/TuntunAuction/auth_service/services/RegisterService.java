33d79f805dfd84ca75591d9c744e4aa2f6fee244
Onibudo Motunrayo <onibudomotunrayo2901@gmail.com>
2026-03-06 00:23:13 +0100

    Logic for the register service

--- src/main/java/com/TuntunAuction/auth_service/services/RegisterService.java
+++ src/main/java/com/TuntunAuction/auth_service/services/RegisterService.java
@@ -1,4 +1,9 @@
 package com.TuntunAuction.auth_service.services;
 
+import com.TuntunAuction.auth_service.dtos.requests.RegisterRequest;
+import com.TuntunAuction.auth_service.dtos.responses.RegisterResponse;
+
 public interface RegisterService {
+
+    RegisterResponse register(RegisterRequest registerRequest);
 }
