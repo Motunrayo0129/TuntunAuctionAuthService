d139746a53e97440dc04b85e62c2cb51c8d6bfda
Onibudo Motunrayo <onibudomotunrayo2901@gmail.com>
2026-03-06 00:21:26 +0100

    Adding the kafka producer for communication

--- src/main/java/com/TuntunAuction/auth_service/services/KafkaProducerService.java
+++ src/main/java/com/TuntunAuction/auth_service/services/KafkaProducerService.java
@@ -1,4 +1,31 @@
 package com.TuntunAuction.auth_service.services;
 
-public class KafkaProducerervices {
+import com.TuntunAuction.auth_service.data.models.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendUserRegisteredEvent(User user, String verificationCode) {
+        Map<String, Object> event = new HashMap<>();
+        event.put("userId", user.getId());
+        event.put("fullname", user.getFullname());
+        event.put("email", user.getEmail());
+        event.put("verificationCode", user.getVerificationCode());
+
+        kafkaTemplate.send("user-registered", event);
+        log.info("User registered event sent for: {}", user.getEmail());
+    }
+
 }
