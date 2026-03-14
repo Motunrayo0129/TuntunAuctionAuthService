97fb4f19efac493abeb4f76f0569fac944a18c2e
Onibudo Motunrayo <onibudomotunrayo2901@gmail.com>
2026-03-14 20:02:44 +0100

    General cleanup to improve code quality

--- src/main/java/com/TuntunAuction/auth_service/controllers/LoginController.java
+++ src/main/java/com/TuntunAuction/auth_service/controllers/LoginController.java
@@ -1,4 +1,30 @@
 package com.TuntunAuction.auth_service.controllers;
 
+import com.TuntunAuction.auth_service.dtos.requests.LoginRequest;
+import com.TuntunAuction.auth_service.dtos.responses.LoginResponse;
+import com.TuntunAuction.auth_service.services.LoginService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+@RequiredArgsConstructor
+
 public class LoginController {
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request){
+        LoginResponse response = loginService.login(request);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+    }
+
+
 }
